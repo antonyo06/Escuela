af9dc69592a157205b48ab2830d57e6879177873
antoniofr <joseantoniofloresromero@hotmail.com>
2020-10-20 15:10:22 -0500

    prueba1

---
+++ database/migrations/2020_10_09_153746_create_empleados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleado', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apaterno');
+            $table->string('amaterno');
+            $table->string('puesto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
